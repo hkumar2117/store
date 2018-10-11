152a24ffd1180b1d3d756a3d72cda48f85e26fe6
Anujkm1991 <anuj662@gmail.com>
2018-10-11 11:08:18 +0530

    MY MESSAGE HERE

--- application/modules/header/views/header.php
+++ application/modules/header/views/header.php
@@ -9,7 +9,8 @@
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-    <script type="text/javascript" src="assets/js/jquery-3.2.1.min.js"></script>   
+    <script type="text/javascript" src="assets/js/jquery-3.2.1.min.js"></script>  
+     <script type="text/javascript" src="assets/js/login_register.js"></script> 
 </head>
 
 <body>
